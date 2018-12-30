2ca506a66c59e7d3ffdd6663d41e841989f1ec03
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-12-30 09:19:38 +0100

    :file_folder: Move tmux into it's own folder

---
+++ tmux/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || return 1
+
+ln -sf "$SCRIPT_DIR/tmux.conf" ~/.tmux.conf
+
+[[ $(uname) == *Darwin* ]] && ln -sf "$SCRIPT_DIR/tmux_osx.conf" ~/.tmux_osx.conf
+
+# tpm
+[[ ! -a ~/.tmux/plugins/tpm ]] && git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
