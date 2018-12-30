4d0ac0692ca7fed6f1dc477f49f7ab51c009dbd6
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-12-30 09:12:52 +0100

    :file_folder: Move aliases, function and env into zsh dir

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,7 +3,12 @@
 # get the dir of the current script
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || return 1
 
+ln -sf "$SCRIPT_DIR/zshrc"         ~/.zshrc
+ln -sf "$SCRIPT_DIR/zsh_env"       ~/.zsh_env
+ln -sf "$SCRIPT_DIR/zsh_aliases"   ~/.zsh_aliases
+ln -sf "$SCRIPT_DIR/zsh_keybinds"  ~/.zsh_keybinds
 ln -sf "$SCRIPT_DIR/zsh_misc"      ~/.zsh_misc
+ln -sf "$SCRIPT_DIR/zsh_functions"    ~/.zsh_functions
 ln -sf "$SCRIPT_DIR/zsh_plug"      ~/.zsh_plug
 ln -sf "$SCRIPT_DIR/zsh_theme"     ~/.zsh_theme
 ln -sf "$SCRIPT_DIR/zsh_fzf_extra" ~/.zsh_fzf_extra
