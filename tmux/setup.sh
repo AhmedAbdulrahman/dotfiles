040ee515992773d2a2326f1b5c0230e8867f73e2
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-02-19 06:40:49 +0100

    :wrench: Update Tmux setup script

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -2,9 +2,16 @@
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || return 1
 
-ln -sf "$SCRIPT_DIR/tmux.conf" ~/.tmux.conf
+mkdir -p ~/.tmux
+mkdir -p ~/.tmux/cache
+# mkdir -p ~/.tmux/config
+
+ln -sf "$SCRIPT_DIR/tmux.conf"  ~/.tmux.conf
+# ln -sf "$SCRIPT_DIR/tmux/config/"  ~/.tmux/config/
 
 [[ $(uname) == *Darwin* ]] && ln -sf "$SCRIPT_DIR/tmux_osx.conf" ~/.tmux_osx.conf
 
-# tpm
-[[ ! -a ~/.tmux/plugins/tpm ]] && git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+for dir in $(ls -d "$SCRIPT_DIR"/*/); do ln -s "$SCRIPT_DIR/$(basename "$dir")" "$HOME/.tmux/$(basename "$dir")"; done && ls -al "$HOME/.tmux/"
+# for dir in "$SCRIPT_DIR"/*/; do
+#   ln -sf "$SCRIPT_DIR/$(basename "$dir")" "$HOME/.tmux/$(basename "$dir")"
+# done
