02b021e64ba4b3b3831a8f1eea3915b5899da284
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-10-27 09:28:33 +0100

    :zap: [Config] adds bat & karabiner config

--- config/setup.sh
+++ config/setup.sh
@@ -8,34 +8,17 @@
 # get the dir of the current script
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || return 1
 
-echo "Create some required folders...\\n"
+echo "→ Creating ~/.config folder..\\n"
 mkdir -p $HOME/.config
 
 echo "⏳Symlink .gitconfig, .gitconfig.local, .gitignore files"
 stow --restow -vv --target="$HOME" --dir=$SCRIPT_DIR git
 
-# Install Taskwarrior, assumes brew is installed.
-echo "⏳Installing Taskwarrior"
-brew="/usr/local/bin/brew"
-
-packages=(
-"task"
-"tasksh"
-"timewarrior"
-)
-
-for package in "${packages[@]}"
-do
-    echo "⏳Installing {$package}"
-    brew install $package
-    echo "---------------------------------------------------------"
-done
-
-echo "⏳Symlink .taskrc file"
+echo "⏳Symlink taskwarrior config"
 stow --restow -vv --target="$HOME" --dir=$SCRIPT_DIR taskwarrior
 
-echo "Create timewarrior folder...\\n"
+echo "→ Creating timewarrior folder...\\n"
 mkdir -p $HOME/.timewarrior
 
-echo "⏳Symlink timewarrior.cfg file"
+echo "⏳Symlink timewarrior config"
 stow --restow -vv --target="$HOME/.timewarrior" --dir=$SCRIPT_DIR timewarrior
