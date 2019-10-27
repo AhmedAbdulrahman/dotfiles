5e4e37d0a569820612373eafe17c534c14fcd0d5
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-10-27 21:37:14 +0100

    :see_no_evil: [Hammerspoon] update setup and ignore setup file when symlinking

--- hammerspoon/setup.sh
+++ hammerspoon/setup.sh
@@ -5,13 +5,4 @@
 #  Created: 2018-12-30 09:25
 ################################################################################
 
-# get the dir of the current script
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-
-# -p says to create the dir if it doesn't exist already.
 mkdir -p ~/.hammerspoon
-
-ln -sf "$SCRIPT_DIR/init.lua"       ~/.hammerspoon/init.lua
-ln -sf "$SCRIPT_DIR/keyboard.lua"   ~/.hammerspoon/keyboard.lua
-ln -sf "$SCRIPT_DIR/position.lua"   ~/.hammerspoon/position.lua
-ln -sf "$SCRIPT_DIR/watcher.lua"    ~/.hammerspoon/watcher.lua
