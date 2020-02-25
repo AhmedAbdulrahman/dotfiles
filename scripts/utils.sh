ac732412bc241d171b7283b8a4712e734f7ac31e
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-02-25 12:42:00 +0100

    :zap: [Scripts/Utils] adds brew file path and analytics
    Signed-off-by: Ahmed Abdulrahman <a.kasapbashi@live.com>

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -456,3 +456,43 @@ brew_upgrade() {
         "Homebrew (upgrade)"
 
 }
+
+get_homebrew_git_config_file_path() {
+
+    local path=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if path="$(brew --repository 2> /dev/null)/.git/config"; then
+        printf "%s" "$path"
+        return 0
+    else
+        print_error "Homebrew (get config file path)"
+        return 1
+    fi
+
+}
+
+brew_opt_out_of_analytics() {
+
+    local path=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Try to get the path of the `Homebrew` git config file.
+
+    path="$(get_homebrew_git_config_file_path)" \
+        || return 1
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Opt-out of Homebrew's analytics.
+    # https://github.com/Homebrew/brew/blob/0c95c60511cc4d85d28f66b58d51d85f8186d941/share/doc/homebrew/Analytics.md#opting-out
+
+    if [ "$(git config --file="$path" --get homebrew.analyticsdisabled)" != "true" ]; then
+        git config --file="$path" --replace-all homebrew.analyticsdisabled true &> /dev/null
+    fi
+
+    print_result $? "Homebrew (opt-out of analytics)"
+
+}
