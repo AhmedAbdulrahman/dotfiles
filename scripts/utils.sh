a15fb275603528a799103e2a06d2ebea9dc8085e
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-02-26 23:10:51 +0100

    :zap: [Scripts/utils] adds helper for setting up github ssh key automaticaly :D
    Signed-off-by: Ahmed Abdulrahman <a.kasapbashi@live.com>

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -598,3 +598,73 @@ create_vimrc_local() {
 
     print_result $? "$FILE_PATH"
 }
+
+# Setup Github SSH Key
+add_ssh_configs() {
+    printf "%s\n" \
+        "Host github.com" \
+        "  IdentityFile $1" \
+        "  LogLevel ERROR" >> ~/.ssh/config
+
+    print_result $? "Add SSH configs"
+
+}
+
+copy_public_ssh_key_to_clipboard () {
+    if cmd_exists "pbcopy"; then
+        pbcopy < "$1"
+        print_result $? "Copy public SSH key to clipboard"
+
+    elif cmd_exists "xclip"; then
+        xclip -selection clip < "$1"
+        print_result $? "Copy public SSH key to clipboard"
+    else
+        print_warning "Please copy the public SSH key ($1) to clipboard"
+    fi
+
+}
+
+generate_ssh_keys() {
+    ask "Please provide an email address: " && printf "\n"
+    ssh-keygen -t rsa -b 4096 -C "$(get_answer)" -f "$1"
+    print_result $? "Generate SSH keys"
+
+}
+
+open_github_ssh_page() {
+    declare -r GITHUB_SSH_URL="https://github.com/settings/ssh"
+
+    # The order of the following checks matters
+    if cmd_exists "xdg-open"; then
+        xdg-open "$GITHUB_SSH_URL"
+    elif cmd_exists "open"; then
+        open "$GITHUB_SSH_URL"
+    else
+        print_warning "Please add the public SSH key to GitHub ($GITHUB_SSH_URL)"
+    fi
+
+}
+
+set_github_ssh_key() {
+    local sshKeyFileName="$HOME/.ssh/github"
+    # If there is already a file with that name, generate another, unique, file name.
+    if [ -f "$sshKeyFileName" ]; then
+        sshKeyFileName="$(mktemp -u "$HOME/.ssh/github_XXXXX")"
+    fi
+
+    generate_ssh_keys "$sshKeyFileName"
+    add_ssh_configs "$sshKeyFileName"
+    copy_public_ssh_key_to_clipboard "${sshKeyFileName}.pub"
+    open_github_ssh_page
+    test_ssh_connection \
+        && rm "${sshKeyFileName}.pub"
+
+}
+
+test_ssh_connection() {
+    while true; do
+        ssh -T git@github.com &> /dev/null
+        [ $? -eq 1 ] && break
+        sleep 5
+    done
+}
