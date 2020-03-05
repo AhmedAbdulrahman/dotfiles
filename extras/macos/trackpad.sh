979a12efb812506d2a162b174e43504669b85eb1
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-03-05 17:24:31 +0100

    :wrench: [macOS] update trackpad defaults
    Signed-off-by: Ahmed Abdulrahman <a.kasapbashi@live.com>

--- extras/macos/trackpad.sh
+++ extras/macos/trackpad.sh
@@ -6,13 +6,15 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n • Trackpad\n\n"
 
 execute "defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true && \
-        defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1 && \
-        defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1" \
-    "Trackpad: enable tap to click for this user and for the login screen"
+         defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1 && \
+         defaults write -g com.apple.mouse.tapBehavior -int 1 && \
+         defaults -currentHost write -g com.apple.mouse.tapBehavior -int 1" \
+    "Enable 'Tap to click'"
 
-execute "defaults write com.apple.BluetoothAudioAgent 'Apple Bitpool Min (editable)' -int 40" \
-    "Increase sound quality for Bluetooth headphones/headsets"
-
-execute "defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true && \
-        defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144" \
-    "Use scroll gesture with the Ctrl (^) modifier key to zoom"
+execute "defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true && \
+         defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -int 1 && \
+         defaults -currentHost write -g com.apple.trackpad.enableSecondaryClick -bool true && \
+         defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 0 && \
+         defaults write com.apple.AppleMultitouchTrackpad TrackpadCornerSecondaryClick -int 0 && \
+         defaults -currentHost write -g com.apple.trackpad.trackpadCornerClickBehavior -int 0" \
+    "Map 'click or tap with two fingers' to the secondary click"
