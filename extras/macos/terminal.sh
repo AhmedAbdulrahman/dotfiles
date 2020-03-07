8877d6ab1f807885a576a2f2e2a8f4f6e9254496
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-03-07 11:54:28 +0100

    :wrench: [macOS] update iTerm2 default script, add Aylin Theme
    Signed-off-by: Ahmed Abdulrahman <a.kasapbashi@live.com>

--- extras/macos/terminal.sh
+++ extras/macos/terminal.sh
@@ -5,26 +5,58 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n • Terminal\n\n"
 
-execute "defaults write com.apple.terminal FocusFollowsMouse -string true" \
-    "Make the focus automatically follow the mouse"
+execute "defaults write com.googlecode.iterm2 PromptOnQuit -bool false" \
+    "Don’t display the annoying prompt when quitting iTerm"
 
-execute "defaults write com.apple.terminal SecureKeyboardEntry -bool true" \
-    "Enable 'Secure Keyboard Entry'"
+execute "defaults write com.googlecode.iterm2 AllowClipboardAccess -bool true" \
+    "Allow terminal apps to access the pasteboard"
 
-execute "defaults write com.apple.Terminal ShowLineMarks -int 0" \
-    "Hide line marks"
+execute "defaults write com.googlecode.iterm2 OnlyWhenMoreTabs -bool false" \
+    "Confirm close window when there are multiple tabs"
 
-execute "defaults write com.apple.terminal StringEncodings -array 4" \
-    "Only use UTF-8"
+execute "defaults write com.googlecode.iterm2 CheckTestRelease -bool true" \
+    "Check for beta versions for auto update"
 
-execute "./set_terminal_theme.applescript" \
-    "Set custom terminal theme"
+execute "defaults write com.googlecode.iterm2 DimBackgroundWindows -bool true" \
+    "Dim inactive windows"
+
+execute "defaults write com.googlecode.iterm2 HideTab -bool false" \
+    "Hide tab bar when there is only one tab"
+
+execute "defaults write com.googlecode.iterm2 IRMemory -int 4" \
+    "Memory (in megabytes) to use per session for instant replay"
+
+execute "defaults write com.googlecode.iterm2 OpenArrangementAtStartup -bool false" \
+    "Open default arrangement at startup"
+
+execute "defaults write com.googlecode.iterm2 OpenNoWindowsAtStartup -bool false" \
+    "Restore only hotkey window at startup"
+
+execute "defaults write com.googlecode.iterm2 SUEnableAutomaticChecks -bool true" \
+    "Automatically check for new versions of iTerm2"
+
+execute "defaults write com.googlecode.iterm2 SavePasteHistory -bool false" \
+    "Should paste and command history be saved to disk"
+
+execute "defaults write com.googlecode.iterm2 SplitPaneDimmingAmount -string '0.4070612980769232'" \
+    "How much to dim inactive split panes or inactive windows"
+
+execute "defaults write com.googlecode.iterm2 StatusBarPosition -integer 1" \
+    "Where does the status bar go"
+
+execute "defaults write com.googlecode.iterm2 UseBorder -bool true" \
+    "Show a border around windows"
+
+execute "defaults write com.googlecode.iterm2 WordCharacters -string '/-+\\\\~-integer.'" \
+    "Characters considered part of a word for selection"
+
+execute "defaults write com.googlecode.iterm2 TabStyleWithAutomaticOption -integer 4" \
+    "Set Tab Style to Automatic"
 
 # Ensure the Touch ID is used when `sudo` is required.
-
 if ! grep -q "pam_tid.so" "/etc/pam.d/sudo"; then
     execute "sudo sh -c 'echo \"auth sufficient pam_tid.so\" >> /etc/pam.d/sudo'" \
         "Use Touch ID to authenticate sudo"
 fi
 
-osascript -e 'quit app "Terminal"'
+osascript -e 'quit app "iTerm"'
