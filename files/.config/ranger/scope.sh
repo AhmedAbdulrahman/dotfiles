36634746d5c2a6794f94592b328499196114fade
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-04-17 00:15:43 +0200

    :wrench: [files/ranger] update files extension

--- files/.config/ranger/scope.sh
+++ files/.config/ranger/scope.sh
@@ -1,34 +1,30 @@
 #!/usr/bin/env bash
+# ranger supports enhanced previews.  If the option "use_preview_script"
+# is set to True and this file exists, this script will be called and its
+# output is displayed in ranger.  ANSI color codes are supported.
 
-set -o noclobber -o noglob -o nounset -o pipefail
-IFS=$'\n'
-
-# If the option `use_preview_script` is set to `true`,
-# then this script will be called and its output will be displayed in ranger.
-# ANSI color codes are supported.
-# STDIN is disabled, so interactive scripts won't work properly
-
-# This script is considered a configuration file and must be updated manually.
-# It will be left untouched if you upgrade ranger.
+# NOTES: This script is considered a configuration file.  If you upgrade
+# ranger, it will be left untouched. (You must update it yourself.)
+# Also, ranger disables STDIN here, so interactive scripts won't work properly
 
 # Meanings of exit codes:
 # code | meaning    | action of ranger
 # -----+------------+-------------------------------------------
-# 0    | success    | Display stdout as preview
-# 1    | no preview | Display no preview at all
-# 2    | plain text | Display the plain content of the file
-# 3    | fix width  | Don't reload when width changes
-# 4    | fix height | Don't reload when height changes
-# 5    | fix both   | Don't ever reload
-# 6    | image      | Display the image `$IMAGE_CACHE_PATH` points to as an image preview
-# 7    | image      | Display the file directly as an image
+# 0    | success    | success. display stdout as preview
+# 1    | no preview | failure. display no preview at all
+# 2    | plain text | display the plain content of the file
+# 3    | fix width  | success. Don't reload when width changes
+# 4    | fix height | success. Don't reload when height changes
+# 5    | fix both   | success. Don't ever reload
+# 6    | image      | success. display the image $cached points to as an image preview
+# 7    | image      | success. display the file directly as an image
 
-# Script arguments
+# Meaningful aliases for arguments:
 path="$1"            # Full path of the selected file
 width="$2"           # Width of the preview pane (number of fitting characters)
 height="$3"          # Height of the preview pane (number of fitting characters)
 cached="$4"          # Path that should be used to cache image previews
-preview_images="$5"  # "True" if image previews are enabled, "False" otherwise
+preview_images="$5"  # "True" if image previews are enabled, "False" otherwise.
 
 maxln=200    # Stop after $maxln lines.  Can be used like ls | head -n $maxln
 
@@ -63,46 +59,40 @@ if [ "$preview_images" = "True" ]; then
     esac
 fi
 
-case "${extension}" in
-    # Archive
+case "$extension" in
+    # Archive extensions:
     a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
     rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
-        atool --list -- "${path}" && exit 5
-        bsdtar --list --file "${path}" && exit 5
+        try als "$path" && { dump | trim; exit 0; }
+        try acat "$path" && { dump | trim; exit 3; }
+        try bsdtar -lf "$path" && { dump | trim; exit 0; }
         exit 1;;
+    csv)
+	sed "s/\(.*\".*\),\(.*\".*\)/\1~\2/;s/,/\t/g;s/~/,/g;s/\t\"/\t/g;s/\"\t/\t/g" "$path" && { dump| trim; exit 0; } || exit 1;;
     rar)
-        # Avoid password prompt by providing empty password
+        # avoid password prompt by providing empty password
         try unrar -p- lt "$path" && { dump | trim; exit 0; } || exit 1;;
     7z)
-        # Avoid password prompt by providing empty password
+        # avoid password prompt by providing empty password
         try 7z -p l "$path" && { dump | trim; exit 0; } || exit 1;;
-
-    # PDF
+    # PDF documents:
     pdf)
-        # Preview as text conversion
-        pdftotext -l 10 -nopgbrk -q -- "${path}" - | fmt -w ${width} && exit 5
-        mutool draw -F txt -i -- "${path}" 1-10 | fmt -w ${width} && exit 5
-        exiftool "${path}" && exit 5
-        exit 1;;
-
-    # BitTorrent
+	try pdftoppm -jpeg -singlefile "$path" "${cached//.jpg}" && exit 6 || exit 1;;
+        #try pdftotext -l 10 -nopgbrk -q "$path" - && \
+            #{ dump | trim | fmt -s -w $width; exit 0; } || exit 1;;
+    # BitTorrent Files
     torrent)
         try transmission-show "$path" && { dump | trim; exit 5; } || exit 1;;
-
-    # OpenDocument
+    # ODT Files
     odt|ods|odp|sxw)
-        # Preview as text conversion
-        odt2txt "${path}" && exit 5
-        exit 1;;
-
-    # HTML
+        try odt2txt "$path" && { dump | trim; exit 5; } || exit 1;;
+    # HTML Pages:
     htm|html|xhtml)
-        # Preview as text conversion
-        w3m -dump "${path}" && exit 5
-        elinks -dump "${path}" && exit 5
-        ;; # Continue with next handler on failure
+        try w3m    -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
+        try lynx   -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
+        try elinks -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
+        ;; # fall back to highlight/cat if the text browsers fail
 esac
-
 
 case "$mimetype" in
     # Syntax highlight for text files:
