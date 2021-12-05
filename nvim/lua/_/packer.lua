f8cca71976d3947245dfee46a58badbd8be36c79
Ahmed Abdulrahman <a.kasapbashi@live.com>
2021-12-05 18:45:09 +0100

    chore(nvim): lazy load friendly snippets

--- nvim/lua/_/packer.lua
+++ nvim/lua/_/packer.lua
@@ -170,7 +170,7 @@ packer.startup {
 			map["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "250" } }
 			map["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "250" } }
 			map["<C-y>"] = { "scroll", { "-0.10", "false", "80" } }
-			map["<C-e>"] = { "scroll", { "0.10", "false", "80" } }
+			-- map["<C-e>"] = { "scroll", { "0.10", "false", "80" } }
 			map["zt"] = { "zt", { "150" } }
 			map["zz"] = { "zz", { "150" } }
 			map["zb"] = { "zb", { "150" } }
@@ -315,6 +315,7 @@ packer.startup {
 	}
 	use {
 	  'L3MON4D3/LuaSnip',
+	  config = require '_.config.snippets',
 	  requires = {
 		{ 'rafamadriz/friendly-snippets' },
 	  },
