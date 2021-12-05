73aa7f3dd4e95049c6f0beeec79e9b1c0b5f4fb6
Ahmed Abdulrahman <a.kasapbashi@live.com>
2021-12-05 19:42:58 +0100

    chore(nvim): adds git browse & git status mappings

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,18 +1,23 @@
-if vim.fn.exists 'g:loaded_fugitive' == 0 then
+if vim.fn.exists('g:loaded_fugitive') == 0 then
   return
 end
 
-local map = require '_.utils.map'
-local au = require '_.utils.au'
+local map = require('_.utils.map')
+local au = require('_.utils.au')
 
-
-if vim.fn.exists 'g:fugitive_browse_handlers' == 0 then
+if vim.fn.exists('g:fugitive_browse_handlers') == 0 then
   vim.g.fugitive_browse_handlers = {}
 end
 
 -- Define mappings.
-map.nnoremap('<leader>gb', '<Cmd>Git blame<CR>', { silent = true })
-map.nnoremap('<leader>gd', '<Cmd>Gdiffsplit<CR>', { silent = true })
+map.nnoremap('gB', '<Cmd>Git blame<CR>', { silent = true })
+map.nnoremap('gd', '<Cmd>Gdiffsplit<CR>', { silent = true })
+-- Open current file on github.com
+map.nnoremap('gb', ':GBrowse<cr>', { silent = true })
+map.vnoremap('gb', ':GBrowse<cr>', { silent = true })
+-- Git status
+map.nnoremap('gs', ':Git<cr>', { silent = true })
+map.vnoremap('gs', ':Git<cr>', { silent = true })
 
 au.augroup('__my_fugitive__', function()
   -- http://vimcasts.org/episodes/fugitive-vim-browsing-the-git-object-database/
