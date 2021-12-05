9b2608f01e22721aab8aa1d3b670ea131a8f94c7
Ahmed Abdulrahman <a.kasapbashi@live.com>
2021-12-05 18:50:13 +0100

    chore(nvim): remap C-e -> C-l in insert mode

--- nvim/plugin/mappings/insert.lua
+++ nvim/plugin/mappings/insert.lua
@@ -1,5 +1,5 @@
-local map = require '_.utils.map'
-local au = require '_.utils.au'
+local map = require('_.utils.map')
+local au = require('_.utils.au')
 
 -- Move lines
 -- map.inoremap('<S-Down>', '<Esc>:m .+1<CR>==gi')
@@ -24,7 +24,7 @@ map.imap('jj', '<esc>')
 map.imap('kk', '<esc>')
 
 map.inoremap('<Down>', '<ESC><Down>', {
-	silent = true,
+  silent = true,
 })
 
 -- when pairing some braces or quotes, put cursor between them
@@ -40,7 +40,7 @@ map.inoremap('<Down>', '<ESC><Down>', {
 map.inoremap('<C-s>', '<Esc>:write<Cr>')
 
 -- This keybinding allows you to jump to the end of the line and we are switched back to insert mode
-map.inoremap('<C-e>', '<C-o>$')
+map.inoremap('<C-l>', '<C-o>$')
 
 -- This keybinding allows you to instead jump to beginning of a line while in insert mode.
 map.inoremap('<C-a>', '<C-o>0')
@@ -48,16 +48,16 @@ map.inoremap('<C-a>', '<C-o>0')
 -- https://twitter.com/vimgifs/status/913390282242232320
 -- :h i_CTRL-G_u
 au.augroup('__prose_mappings__', function()
-	au.autocmd('FileType', 'markdown,text', function()
-	  map.inoremap('.', '.<c-g>u', { buffer = true })
-	end)
-	au.autocmd('FileType', 'markdown,text', function()
-	  map.inoremap('?', '?<c-g>u', { buffer = true })
-	end)
-	au.autocmd('FileType', 'markdown,text', function()
-	  map.inoremap('!', '!<c-g>u', { buffer = true })
-	end)
-	au.autocmd('FileType', 'markdown,text', function()
-	  map.inoremap(',', ',<c-g>u', { buffer = true })
-	end)
+  au.autocmd('FileType', 'markdown,text', function()
+    map.inoremap('.', '.<c-g>u', { buffer = true })
+  end)
+  au.autocmd('FileType', 'markdown,text', function()
+    map.inoremap('?', '?<c-g>u', { buffer = true })
+  end)
+  au.autocmd('FileType', 'markdown,text', function()
+    map.inoremap('!', '!<c-g>u', { buffer = true })
+  end)
+  au.autocmd('FileType', 'markdown,text', function()
+    map.inoremap(',', ',<c-g>u', { buffer = true })
+  end)
 end)
