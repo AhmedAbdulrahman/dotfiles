ec21d790acb5544a26ca43b31221c715d47ca410
Ahmed Abdulrahman <a.kasapbashi@live.com>
2021-12-19 00:28:35 +0100

    chore(nvim): remove unused variable map

--- nvim/after/ftplugin/javascript.lua
+++ nvim/after/ftplugin/javascript.lua
@@ -1,16 +1,14 @@
-local map = require '_.utils.map'
-
-vim.cmd [[setlocal conceallevel=2]]
-vim.cmd [[setlocal isfname+=@-@ ]]
-vim.cmd [[setlocal suffixesadd=.js,.jsx ]]
+vim.cmd([[setlocal conceallevel=2]])
+vim.cmd([[setlocal isfname+=@-@ ]])
+vim.cmd([[setlocal suffixesadd=.js,.jsx ]])
 
 local package_lock = vim.fn.findfile(
   'package-lock.json',
-  vim.fn.expand '%:p' .. ';'
+  vim.fn.expand('%:p') .. ';'
 )
 
 if vim.fn.filereadable(package_lock) == 1 then
-  vim.cmd [[setlocal makeprg=npm]]
+  vim.cmd([[setlocal makeprg=npm]])
 else
-  vim.cmd [[setlocal makeprg=yarn]]
+  vim.cmd([[setlocal makeprg=yarn]])
 end
