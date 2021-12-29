fa3deca1dc5e9244b6191ab5d1cec4c17140f855
Ahmed Abdulrahman <a.kasapbashi@live.com>
2021-12-30 00:23:52 +0100

    chore(zsh): add Docker alias and functions

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -107,6 +107,26 @@ alias tl='tmux list-sessions'
 [[ $TERM == *"tmux"* ]] && alias brew="TERM=xterm-256color brew"
 
 # ---------------------------------------------
+# Docker
+# ---------------------------------------------
+# Get latest container ID
+alias dl="docker ps -l -q"
+# Get container process
+alias dps="docker ps"
+# Get process included stop container
+alias dpa="docker ps -a"
+# Get images
+alias di="docker images"
+# Get container IP
+alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
+# Run deamonized container, e.g., $dkd base /bin/echo hello
+alias dkd="docker run -d -P"
+# Run interactive container, e.g., $dki base /bin/bash
+alias dki="docker run -i -t -P"
+# Execute interactive container, e.g., $dex base /bin/bash
+alias dex="docker exec -i -t"
+
+# ---------------------------------------------
 # Utilities
 # ---------------------------------------------
 alias "?"="pwd"
