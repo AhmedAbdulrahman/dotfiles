66452340f17a40194af8e1c9f933c8602986a1e8
Ahmed Abdulrahman <a.kasapbashi@live.com>
2022-03-12 16:55:01 +0100

    feat(nvim): don't yank on visual paste

--- nvim/plugin/mappings/visual.lua
+++ nvim/plugin/mappings/visual.lua
@@ -1,4 +1,4 @@
-local map = require 'utils.map'
+local map = require('utils.map')
 local opts = { noremap = true, silent = true }
 
 -- https://github.com/mhinz/vim-galore#dont-lose-selection-when-shifting-sidewards
@@ -13,6 +13,7 @@ map.vnoremap('>', '>gv', opts)
 map.vnoremap('x', '"_x', opts)
 map.vnoremap('X', '"_X', opts)
 
+-- Don't yank on visual paste
 map.vnoremap('p', '"_dP', opts)
 
 -- Make dot work in visual mode
@@ -22,19 +23,19 @@ map.vnoremap('.', ':norm.<CR>')
 map.xnoremap('ga', '<Plug>(EasyAlign)', { silent = true })
 
 map.vmap('<Leader>hu', ':call utils#HtmlUnEscape()<cr>', {
-	silent = true,
+  silent = true,
 })
 
 map.vmap('<Leader>he', ':call utils#HtmlEscape()<cr>', {
-	silent = true,
+  silent = true,
 })
 
 -- Allows you to visually select a section and then hit @ to run a macro on all lines
 -- https://medium.com/@schtoeffel/you-don-t-need-more-than-one-cursor-in-vim-2c44117d51db#.3dcn9prw6
-vim.cmd [[function! ExecuteMacroOverVisualRange()
+vim.cmd([[function! ExecuteMacroOverVisualRange()
 	echo '@'.getcmdline()
 	execute ":'<,'>normal @".nr2char(getchar())
-  endfunction]]
+  endfunction]])
 
 map.xnoremap('@', ':<C-u>call ExecuteMacroOverVisualRange()<CR>')
 
@@ -53,8 +54,16 @@ map.xnoremap('?', '?\v')
 map.xnoremap('<leader>r', ':<c-u>%s/\\%V')
 
 -- Move highlighted lines
-map.xnoremap('K', ':call mappings#visual#movelines#moveup()<CR>', { silent = true })
-map.xnoremap('J', ':call mappings#visual#movelines#movedown()<CR>', { silent = true })
+map.xnoremap(
+  'K',
+  ':call mappings#visual#movelines#moveup()<CR>',
+  { silent = true }
+)
+map.xnoremap(
+  'J',
+  ':call mappings#visual#movelines#movedown()<CR>',
+  { silent = true }
+)
 
 -- -- Move selected line / block of text in visual mode
 -- map.xnoremap('K', ":move '<-2<CR>gv-gv", opts)
