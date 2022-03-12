e2e44117d2120b1e64cb46e929e6081d723279fa
Ahmed Abdulrahman <a.kasapbashi@live.com>
2022-03-12 17:45:25 +0100

    fix(nvim): disable space and eol chars

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -1,7 +1,6 @@
-vim.opt.listchars = {
-  space = '⋅',
-  eol = '↴',
-}
+vim.opt.list = true
+-- vim.opt.listchars:append("space:⋅")
+-- vim.opt.listchars:append("eol:↴")
 
 require('indent_blankline').setup({
   buftype_exclude = { 'terminal', 'telescope', 'nofile' },
@@ -23,8 +22,10 @@ require('indent_blankline').setup({
     'neogitstatus',
     '',
   },
-  space_char_blankline = ' ',
   show_current_context = true,
-  show_end_of_line = true,
+  show_current_context_start = false,
+  show_end_of_line = false,
+  show_trailing_blankline_indent = false,
+  space_char_blankline = ' ',
   use_treesitter = true,
 })
