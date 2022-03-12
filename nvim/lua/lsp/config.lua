42331290ddf2589b2479904633b0a7057864ab10
Ahmed Abdulrahman <a.kasapbashi@live.com>
2022-03-12 17:59:13 +0100

    feat(nvim): add lsp codes & diagnostic source information formats to LSP

--- nvim/lua/lsp/config.lua
+++ nvim/lua/lsp/config.lua
@@ -1,6 +1,33 @@
+-- Diagnostic config
+
+vim.diagnostic.config({
+  float = {
+    format = function(diagnostic)
+      if not diagnostic.source or not diagnostic.user_data.lsp.code then
+        return string.format('%s', diagnostic.message)
+      end
+
+      if diagnostic.source == 'eslint' then
+        return string.format(
+          '%s [%s]',
+          diagnostic.message,
+          diagnostic.user_data.lsp.code
+        )
+      end
+
+      return string.format('%s [%s]', diagnostic.message, diagnostic.source)
+    end,
+  },
+  severity_sort = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  virtual_text = true,
+})
+
 -- UI
 
-local signs = { Error = ' 🞮', Warn = ' ▲', Hint = ' ', Info = ' ' }
+local signs = { Error = ' ', Warn = ' ▲', Hint = ' ', Info = ' ' }
 
 for type, icon in pairs(signs) do
   local hl = 'DiagnosticSign' .. type
