d482677d254cf10d0da61f39904afc9c2195e5f2
Ahmed Abdulrahman <a.kasapbashi@live.com>
2022-03-17 17:33:35 +0100

    feat(nvim/lsp): add native autocmds support

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -4,77 +4,206 @@
 
 -- luacheck: max line length 150
 
-local au = require('utils.au')
+vim.api.nvim_create_augroup('AutoUpdatePlugins', { clear = true })
+vim.api.nvim_create_autocmd(
+  'BufWritePost',
+  {
+    pattern = 'plugins.lua',
+    command = 'source <afile> | PackerSync',
+    group = 'AutoUpdatePlugins',
+  }
+)
+vim.api.nvim_create_autocmd(
+  'BufWritePost',
+  {
+    pattern = 'plugins.lua',
+    command = 'source <afile> | PackerCompile',
+    group = 'AutoUpdatePlugins',
+  }
+)
 
-au.group('__myautocmds__', {
+vim.api.nvim_create_augroup('Highlight', { clear = true })
+vim.api.nvim_create_autocmd(
+  'TextYankPost',
   {
-    'TextYankPost',
-    '*',
-    'silent! lua vim.highlight.on_yank() {higroup="IncSearch", timeout=400}',
-  },
-  { 'BufWritePost', 'plugins.lua', [[source <afile> | PackerCompile]] },
-  { 'BufWritePost', 'plugins.lua', [[source <afile> | PackerSync]] },
-  { 'BufRead', '*/node_modules/*', [[lua vim.diagnostic.disable(0)]] },
-  { 'BufNewFile', '*/node_modules/*', [[lua vim.diagnostic.disable(0)]] },
-  -- { 'BufWritePost', '*', 'silent! FormatWrite' },
-  { 'BufWritePost', '*/spell/*.add', 'silent! :mkspell! %' },
-  { 'BufWinEnter', 'NvimTree', 'let &l:statusline = " Explorer"' },
-  { 'User', 'Startified', 'setlocal cursorline' },
-  { 'BufWinEnter', 'NvimTree', 'let &l:statusline = " Explorer"' },
-  { 'User', 'Startified', 'setlocal cursorline' },
-  -- Save the current buffer after any changes.
+    command = "silent! lua vim.highlight.on_yank() {higroup='IncSearch', timeout=400}",
+    group = 'Highlight',
+  }
+)
+
+vim.api.nvim_create_augroup('LspNodeModules', { clear = true })
+vim.api.nvim_create_autocmd(
+  'BufRead',
   {
-    'InsertLeave,TextChanged',
-    '*',
-    'nnoremap <buffer><leader>p :call autocmds#savebuffer#()',
-  },
-  --  Set current working directory.
-  { 'VimEnter', '*', 'nnoremap <buffer><leader>p :call autocmds#setroot#()' },
-  -- Automatically make splits equal in size
-  { 'VimResized', '*', 'wincmd =' },
-  -- Disable paste mode on leaving insert mode.
-  { 'InsertLeave', '*', 'set nopaste' },
-  -- taken from https://github.com/jeffkreeftmeijer/vim-numbertoggle/blob/cfaecb9e22b45373bb4940010ce63a89073f6d8b/plugin/number_toggle.vim
+    pattern = '*/node_modules/*',
+    command = 'lua vim.diagnostic.disable(0)',
+    group = 'LspNodeModules',
+  }
+)
+vim.api.nvim_create_autocmd(
+  'BufNewFile',
   {
-    'BufLeave,FocusLost,InsertEnter,WinLeave',
-    '*',
-    'if &nu | set nornu | endif',
-  },
+    pattern = '*/node_modules/*',
+    command = 'lua vim.diagnostic.disable(0)',
+    group = 'LspNodeModules',
+  }
+)
+vim.api.nvim_create_autocmd(
+  'BufEnter,WinEnter',
   {
-    'BufEnter,FocusGained,InsertLeave,WinEnter',
-    '*',
-    'if &nu | set rnu   | endif',
-  },
-  { 'BufEnter,WinEnter', '*/node_modules/*', ':LspStop' },
-  { 'BufLeave', '*/node_modules/*', ':LspStart' },
-  { 'BufEnter,WinEnter', '*.min.*', ':LspStop' },
-  { 'BufLeave', '*.min.*', ':LspStart' },
+    pattern = '*/node_modules/*',
+    command = 'LspStop',
+    group = 'LspNodeModules',
+  }
+)
+vim.api.nvim_create_autocmd(
+  'BufLeave',
   {
-    'BufReadPre',
-    '*',
-    'lua require("functions").disable_heavy_plugins()',
-  },
-  { 'FileType', '*', 'lua require("functions").quit_on_q()' },
+    pattern = '*/node_modules/*',
+    command = 'LspStart',
+    group = 'LspNodeModules',
+  }
+)
+vim.api.nvim_create_autocmd(
+  'BufEnter,WinEnte',
+  { pattern = '*.min.*', command = 'LspStop', group = 'LspNodeModules' }
+)
+vim.api.nvim_create_autocmd(
+  'BufLeave',
+  { pattern = '*.min.*', command = 'LspStop', group = 'LspNodeModules' }
+)
+
+vim.api.nvim_create_augroup('Spell', { clear = true })
+vim.api.nvim_create_autocmd(
+  'BufWritePost',
   {
-    'BufWritePost,BufLeave,WinLeave',
-    '?*',
-    'lua require("functions").mkview()',
-  },
-  { 'BufWinEnter', '?*', 'lua require("functions").loadview()' },
+    pattern = '*/spell/*.add',
+    command = 'silent! :mkspell! %',
+    group = 'Spell',
+  }
+)
+
+vim.api.nvim_create_augroup('NvimTree', { clear = true })
+vim.api.nvim_create_autocmd(
+  'BufWinEnter',
   {
-    'BufEnter,BufWinEnter,BufRead,BufNewFile',
-    'bookmarks.{md,txt}',
-    'hi! link mkdLink Normal | set concealcursor-=n',
-  },
-  -- Project specific override
+    pattern = '*/spell/*.add',
+    command = 'let &l:statusline = " Explorer"',
+    group = 'NvimTree',
+  }
+)
+
+vim.api.nvim_create_augroup('Startified', { clear = true })
+vim.api.nvim_create_autocmd(
+  'User',
   {
-    'BufRead,BufNewFile',
-    '*',
-    'lua require("functions").source_project_config()',
-  },
+    pattern = 'Startified',
+    command = 'setlocal cursorline',
+    group = 'Startified',
+  }
+)
+
+vim.api.nvim_create_augroup('__myautocmds__', { clear = true })
+-- Save the current buffer after any changes.
+vim.api.nvim_create_autocmd(
+  'InsertLeave,TextChanged',
   {
-    'DirChanged',
-    '*',
-    'lua require("functions").source_project_config()',
-  },
-})
+    pattern = '*',
+    command = 'nnoremap <buffer><leader>p :call autocmds#savebuffer#()',
+    group = '__myautocmds__',
+  }
+)
+--  Set current working directory.
+vim.api.nvim_create_autocmd(
+  'VimEnter',
+  {
+    pattern = '*',
+    command = 'nnoremap <buffer><leader>p :call autocmds#setroot#()',
+    group = '__myautocmds__',
+  }
+)
+-- Automatically make splits equal in size
+vim.api.nvim_create_autocmd(
+  'VimResized',
+  { pattern = '*', command = 'wincmd =', group = '__myautocmds__' }
+)
+-- Disable paste mode on leaving insert mode.
+vim.api.nvim_create_autocmd(
+  'InsertLeave',
+  { pattern = '*', command = 'set nopaste', group = '__myautocmds__' }
+)
+-- taken from https://github.com/jeffkreeftmeijer/vim-numbertoggle/blob/cfaecb9e22b45373bb4940010ce63a89073f6d8b/plugin/number_toggle.vim
+vim.api.nvim_create_autocmd(
+  'BufLeave,FocusLost,InsertEnter,WinLeave',
+  {
+    pattern = '*',
+    command = 'if &nu | set nornu | endif',
+    group = '__myautocmds__',
+  }
+)
+vim.api.nvim_create_autocmd(
+  'BufEnter,FocusGained,InsertLeave,WinEnter',
+  {
+    pattern = '*',
+    command = 'if &nu | set rnu | endif',
+    group = '__myautocmds__',
+  }
+)
+-- Automatically make splits equal in size
+vim.api.nvim_create_autocmd(
+  'BufReadPre',
+  {
+    pattern = '*',
+    command = 'lua require("functions").disable_heavy_plugins()',
+    group = '__myautocmds__',
+  }
+)
+vim.api.nvim_create_autocmd(
+  'FileType',
+  {
+    pattern = '*',
+    command = 'lua require("functions").quit_on_q()',
+    group = '__myautocmds__',
+  }
+)
+vim.api.nvim_create_autocmd(
+  'BufWritePost,BufLeave,WinLeave',
+  {
+    pattern = '?*',
+    command = 'lua require("functions").mkview()',
+    group = '__myautocmds__',
+  }
+)
+vim.api.nvim_create_autocmd(
+  'BufWinEnter',
+  {
+    pattern = '?*',
+    command = 'lua require("functions").loadview()',
+    group = '__myautocmds__',
+  }
+)
+vim.api.nvim_create_autocmd(
+  'BufEnter,BufWinEnter,BufRead,BufNewFile',
+  {
+    pattern = 'bookmarks.{md,txt}',
+    command = 'hi! link mkdLink Normal | set concealcursor-=n',
+    group = '__myautocmds__',
+  }
+)
+-- Project specific override
+vim.api.nvim_create_autocmd(
+  'BufRead,BufNewFile',
+  {
+    pattern = '*',
+    command = 'lua require("functions").source_project_config()',
+    group = '__myautocmds__',
+  }
+)
+vim.api.nvim_create_autocmd(
+  'DirChanged',
+  {
+    pattern = '*',
+    command = 'lua require("functions").source_project_config()',
+    group = '__myautocmds__',
+  }
+)
