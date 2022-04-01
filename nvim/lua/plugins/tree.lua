4a3d1de85273c28f0831379be7790acaac0eaa2c
Ahmed Abdulrahman <a.kasapbashi@live.com>
2022-04-02 00:07:37 +0200

    chore(nvim): update nvim-tree config

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -109,8 +109,6 @@ require('nvim-tree').setup({
   open_on_setup = false,
   -- will not open on setup if the filetype is in this list
   ignore_ft_on_setup = {},
-  -- closes neovim automatically when the tree is the last **WINDOW** in the view
-  auto_close = false,
   -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
   open_on_tab = false,
   -- hijacks new directory buffers when they are opened.
@@ -206,9 +204,6 @@ require('nvim-tree').setup({
   actions = {
     open_file = {
       quit_on_open = true,
-      window_picker = {
-        enable = true,
-      },
     },
   },
 })
