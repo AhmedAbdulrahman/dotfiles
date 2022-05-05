5d558ba29f0f19ffb9eaed20226535d6f1280fad
Ahmed Abdulrahman <a.kasapbashi@live.com>
2022-05-05 18:57:57 +0200

    fix(lsp): format on save function fix

--- nvim/lua/lsp/functions.lua
+++ nvim/lua/lsp/functions.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 function M.enable_format_on_save()
-  vim.api.nvim_create_augroup("Formatter", { clear = true })
-  vim.api.nvim_create_autocmd("BufWritePre", {
-   pattern = '*',
-   command = 'silent! FormatWrite',
-   group = 'Formatter'
+  local group = vim.api.nvim_create_augroup("Formatter", { clear = false })
+  vim.api.nvim_create_autocmd("BufWritePre", { callback = function()
+    vim.lsp.buf.formatting_sync()
+   end,
+   group = group
   })
   require('notify')("Enabled format on save", "info", { title = "LSP", timeout = 2000 })
 end
