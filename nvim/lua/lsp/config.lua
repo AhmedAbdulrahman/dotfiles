b3c0dfcfe28384bc5fafc9bdbe176ee0ff0c80a5
Ahmed Abdulrahman <a.kasapbashi@live.com>
2022-06-15 09:21:53 +0200

    feat(nvim): change lsp virtual text icon

--- nvim/lua/lsp/config.lua
+++ nvim/lua/lsp/config.lua
@@ -111,7 +111,9 @@ vim.diagnostic.config({
   signs = true,
   underline = true,
   update_in_insert = false,
-  virtual_text = true,
+  virtual_text = {
+    prefix = NvimConfig.icons.circle,
+  },
 })
 
 -- UI
