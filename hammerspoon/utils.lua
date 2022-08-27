8be535a5f235558fd0f62e9a8939e80389443358
Ahmed Abdulrahman <a.kasapbashi@live.com>
2022-08-27 12:36:08 +0200

    feat(hammerspoon): add custom timer module

--- hammerspoon/utils.lua
+++ hammerspoon/utils.lua
@@ -5,4 +5,55 @@ module.capitalize = function(str)
   return str:gsub("^%l", string.upper)
 end
 
+-- split a string
+module.split  = function (str, pat)
+	local t = {}  -- NOTE: use {n = 0} in Lua-5.0
+	local fpat = "(.-)" .. pat
+	local lastEnd = 1
+	local s, e, cap = str:find(fpat, 1)
+	while s do
+	  if s ~= 1 or cap ~= "" then
+		table.insert(t,cap)
+	  end
+	  lastEnd = e+1
+	  s, e, cap = str:find(fpat, lastEnd)
+	end
+	if lastEnd <= #str then
+	  cap = str:sub(lastEnd)
+	  table.insert(t, cap)
+	end
+	return t
+end
+
+-- Return a string that converts a number of minutes into MM:SS format.
+module.prettyMinutes = function (minutes)
+	return string.format('%02d:%02d', math.floor(minutes/60), minutes%60)
+end
+
+-- Create a new counter with the given name and first value.
+-- If the first value is 0 (default), the counter will count upward,
+-- otherwise the counter will count downward.
+module.new  = function (name, begin)
+	local first = begin or 0
+	local amt = first == 0 and 1 or -1
+	local i = first
+	local repr = 'counter (' .. (name or 'unnamed') .. '): '
+
+	return {
+	  -- increment/decrement the counter (depending on first value == 0 or not)
+	  incr = function() i = i + amt end,
+	  -- reset the counter to the first value
+	  reset = function() i = first end,
+	  -- get the current count
+	  get = function() return i end,
+	  -- get a string representation of the counter (useful for debugging)
+	  asString = function() return repr .. i end,
+	}
+end
+
+-- Takes a list of path parts, returns a string with the parts delimited by '/'
+module.toPath = function(...)
+	return table.concat({...}, '/')
+end
+
 return module
