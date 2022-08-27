ffbdecd47b65f50a30fbb2fb9964a00f5dafb8a4
Ahmed Abdulrahman <a.kasapbashi@live.com>
2022-08-27 12:34:46 +0200

    feat(hammerspoon): add new useful modules

---
+++ hammerspoon/brightness/brightness.lua
@@ -0,0 +1,12 @@
+function changeBrightness(diff)
+  return function()
+    local current = hs.brightness.get()
+    local new = math.min(100, math.max(0, math.floor(current + diff)))
+    hs.alert.closeAll(0.0)
+    hs.alert.show('Brightness ' .. new .. '%', {}, 0.5)
+    hs.brightness.set(new)
+  end
+end
+
+hs.hotkey.bind({ 'cmd' }, 'Left', changeBrightness(-3))
+hs.hotkey.bind({ 'cmd' }, 'Right', changeBrightness(3))
