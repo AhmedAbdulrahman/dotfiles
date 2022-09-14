0c36d00bfed2c15e6741ac894e2d145e4683c8d0
Ahmed Abdulrahman <a.kasapbashi@live.com>
2022-09-14 15:44:54 +0200

    feat(lsp): enable formatting in css language server

--- nvim/lua/lsp/run.lua
+++ nvim/lua/lsp/run.lua
@@ -1,4 +1,3 @@
-
 -- Setup installer & lsp configs
 local typescript_ok, typescript = pcall(require, 'typescript')
 
@@ -8,28 +7,48 @@ if not lsp_installer_ok then
   return
 end
 
-lsp_installer.setup {
+lsp_installer.setup({
   -- A list of servers to automatically install if they're not already installed
-  ensure_installed = { "bashls", "cssls", "eslint", "graphql", "html", "jsonls", "sumneko_lua", "tailwindcss", "tsserver", "vetur", "vuels" },
+  ensure_installed = {
+    'bashls',
+    'cssls',
+    'eslint',
+    'graphql',
+    'html',
+    'jsonls',
+    'sumneko_lua',
+    'tailwindcss',
+    'tsserver',
+    'vetur',
+    'vuels',
+  },
   -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed
   automatic_installation = true,
-}
+})
 
-local lspconfig = require("lspconfig")
+local lspconfig = require('lspconfig')
 
 local handlers = {
-  ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = NvimConfig.ui.float.border }),
-  ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = NvimConfig.ui.float.border }),
+  ['textDocument/hover'] = vim.lsp.with(
+    vim.lsp.handlers.hover,
+    { border = NvimConfig.ui.float.border }
+  ),
+  ['textDocument/signatureHelp'] = vim.lsp.with(
+    vim.lsp.handlers.signature_help,
+    { border = NvimConfig.ui.float.border }
+  ),
 }
 
-local function on_attach(client, bufnr)
+local function on_attach()
   -- set up buffer keymaps, etc.
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
 if cmp_nvim_lsp_ok then
-  capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp_nvim_lsp.update_capabilities(
+    vim.lsp.protocol.make_client_capabilities()
+  )
 end
 
 -- Order matters
@@ -45,51 +64,56 @@ if typescript_ok then
       capabilities = require('lsp.servers.tsserver').capabilities,
       handlers = handlers,
       on_attach = require('lsp.servers.tsserver').on_attach,
-    }
+    },
   })
 end
 
-lspconfig.tailwindcss.setup {
+lspconfig.tailwindcss.setup({
   capabilities = require('lsp.servers.tsserver').capabilities,
   filetypes = require('lsp.servers.tailwindcss').filetypes,
   handlers = handlers,
   init_options = require('lsp.servers.tailwindcss').init_options,
   on_attach = require('lsp.servers.tailwindcss').on_attach,
   settings = require('lsp.servers.tailwindcss').settings,
-}
+})
 
-lspconfig.eslint.setup {
+lspconfig.cssls.setup({
+  capabilities = capabilities,
+  handlers = handlers,
+  on_attach = require('lsp.servers.cssls').on_attach,
+})
+
+lspconfig.eslint.setup({
   capabilities = capabilities,
   handlers = handlers,
   on_attach = require('lsp.servers.eslint').on_attach,
   settings = require('lsp.servers.eslint').settings,
-}
+})
 
-lspconfig.jsonls.setup {
+lspconfig.jsonls.setup({
   capabilities = capabilities,
   handlers = handlers,
   on_attach = on_attach,
   settings = require('lsp.servers.jsonls').settings,
-}
+})
 
-lspconfig.sumneko_lua.setup {
+lspconfig.sumneko_lua.setup({
   handlers = handlers,
   on_attach = on_attach,
   settings = require('lsp.servers.sumneko_lua').settings,
-}
+})
 
-lspconfig.vuels.setup {
+lspconfig.vuels.setup({
   filetypes = require('lsp.servers.vuels').filetypes,
   handlers = handlers,
   init_options = require('lsp.servers.vuels').init_options,
   on_attach = on_attach,
-}
+})
 
-
-for _, server in ipairs { "bashls", "cssls", "graphql", "html", "volar" } do
-  lspconfig[server].setup {
+for _, server in ipairs({ 'bashls', 'graphql', 'html', 'volar' }) do
+  lspconfig[server].setup({
     on_attach = on_attach,
     capabilities = capabilities,
     handlers = handlers,
-  }
+  })
 end
--- nvim/lua/lsp/servers/cssls.lua
+++ nvim/lua/lsp/servers/cssls.lua
@@ -2,4 +2,9 @@ local M = {}
 
 M.settings = {}
 
+M.on_attach = function(client)
+  client.server_capabilities.documentFormattingProvider = true
+  client.server_capabilities.documentRangeFormattingProvider = true
+end
+
 return M
