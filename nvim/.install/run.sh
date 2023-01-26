5d5c1015e657ba8550563e148e9592f6b2b67526
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-01-26 08:34:04 +0100

    feat(nvim): migrating from Packer.nvim to Lazy.nvim

--- nvim/.install/run.sh
+++ nvim/.install/run.sh
@@ -9,10 +9,11 @@ NC=$(tput sgr0)
 BOLD=$(tput bold)
 
 declare -r NVIM_DIR="${NVIM_DIR:-"$(which nvim)"}"
+declare -r XDG_DATA_HOME="${XDG_DATA_HOME:-"$HOME/.local/share"}"
+declare -r XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}"
 declare -r RUNTIME_DIR="${RUNTIME_DIR:-"$XDG_DATA_HOME/nvim"}"
 declare -r CONFIG_DIR="${CONFIG_DIR:-"$XDG_CONFIG_HOME/nvim"}"
 declare -r DOTFILES_DIR="$HOME/dotfiles"
-declare -r PACK_DIR="$RUNTIME_DIR/site/pack"
 
 # MAIN
 function main() {
@@ -35,7 +36,7 @@ function main() {
 
 	remove_current_config
 	create_nvim_config_dir
-	install_packer
+	install_lazy
 	setup
 	finish
 }
@@ -161,47 +162,35 @@ function create_nvim_config_dir() {
   echo -e "${GREEN}${BOLD}✓ Done${NC}"
 }
 
-function install_packer() {
-  if [ -e "$PACK_DIR/packer/start/packer.nvim" ]; then
-    msg "${BOLD}${GREEN}Packer already installed!${NC}"
+function install_lazy() {
+  if [ -e "$RUNTIME_DIR/lazy/lazy.nvim" ]; then
+    msg "${BOLD}${GREEN}Lazy.nvim already installed!${NC}"
     echo -e
   else
-    if ! git clone --depth 1 "https://github.com/wbthomason/packer.nvim" \
-      "$PACK_DIR/packer/start/packer.nvim"; then
-      msg "${BOLD}${RED}Failed to clone Packer. Installation failed.${NC}"
+    if ! git clone --filter=blob:none --single-branch "https://github.com/folke/lazy.nvim.git" \
+      "$RUNTIME_DIR/lazy/lazy.nvim"; then
+      msg "${BOLD}${RED}Failed to clone Lazy.nvim. Installation failed.${NC}"
       exit 1
     fi
   fi
 }
 
+function setup() {
+  cd $CONFIG_DIR/.install
+
+  msg "${BOLD}Installing plugins...${NC}" 1
+  nvim --headless "+Lazy! sync" +qa
+
+  msg "${BOLD}${GREEN}Done${NC}" 1 0
+
+  msg "${BOLD}${GREEN}Plugin installation completed!${NC}" 1
+}
+
+
 function finish () {
   touch /tmp/first-nvim-run
   msg "${BOLD}${GREEN}Thank you for installing my ${BLUE}Nvim${NC}${BOLD}${GREEN} config! Please support me by giving a star :)${NC}" 1
   echo -e "${BOLD}${GREEN}Do not forget to use a font with glyphs (icons) support [https://github.com/ryanoasis/nerd-fonts].\nI recommend Fira Code for Nvim setup.${NC}"
-}
-
-function setup() {
-  msg "${BOLD}Installing telescope-fzf-native...${NC}" 1
-  git clone https://github.com/nvim-telescope/telescope-fzf-native.nvim /tmp/telescope-fzf-native.nvim
-  rm -rf /tmp/telescope-fzf-native.nvim/.git
-  cp -r /tmp/telescope-fzf-native.nvim "$PACK_DIR/packer/start/packer.nvim"
-  rm -rf /tmp/telescope-fzf-native.nvim
-  [ -d "$PACK_DIR/packer/start/packer.nvim/telescope-fzf-native.nvim" ] && msg "${BOLD}${GREEN}Done${NC}" 1 0
-  [ ! -d "$PACK_DIR/packer/start/packer.nvim/telescope-fzf-native.nvim" ] && msg "${BOLD}${RED}Error while installing telescope-fzf-native... Aborting" 1 && exit
-
-  cd "$PACK_DIR/packer/start/packer.nvim/telescope-fzf-native.nvim"
-  msg "${BOLD}Building telescope-fzf-native...${NC}"
-  make
-  [ ! -f "$PACK_DIR/packer/start/packer.nvim/telescope-fzf-native.nvim/build/libfzf.so" ] && msg "${BOLD}${RED}Error while building telescope-fzf-native... Aborting" 1 && exit
-  [ -f "$PACK_DIR/packer/start/packer.nvim/telescope-fzf-native.nvim/build/libfzf.so" ] && msg "${BOLD}${GREEN}Done${NC}" 1 0
-  cd $DOTFILES_DIR
-
-  msg "${BOLD}Installing plugins...${NC}" 1
-  nvim -c 'autocmd User PackerComplete quitall' \
-    -c 'PackerSync'
-  msg "${BOLD}${GREEN}Done${NC}" 1 0
-
-  msg "${BOLD}${GREEN}Packer setup complete!${NC}" 1
 }
 
 main
---
+++ nvim/lua/core/plugins.lua
@@ -0,0 +1,455 @@
+return {
+  { 'nvim-lua/plenary.nvim' },
+  { 'kyazdani42/nvim-web-devicons' },
+  {
+    'mhinz/vim-startify',
+    config = function()
+      require('plugins.startify')
+    end,
+    lazy = false,
+  },
+  -- Themes
+  {
+    'AhmedAbdulrahman/aylin.vim',
+    branch = '0.5-nvim',
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.opt.background = 'dark'
+      vim.cmd('colorscheme ' .. NvimConfig.colorscheme)
+    end,
+  },
+  { 'arzg/vim-substrata' },
+
+  -- Treesitter
+  {
+    'nvim-treesitter/nvim-treesitter',
+    event = 'BufReadPre',
+    config = function()
+      require('plugins.treesitter')
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'RRethy/nvim-treesitter-textsubjects',
+      -- 'romgrk/nvim-treesitter-context',
+      -- 'JoosepAlviste/nvim-ts-context-commentstring',
+      -- 'nvim-treesitter/playground',
+      {
+        'm-demare/hlargs.nvim',
+        config = function()
+          require('hlargs').setup({ color = '#F7768E' })
+        end,
+      },
+    },
+  },
+
+  -- 🔭 Navigating (Telescope/Tree/Refactor)
+  {
+    'nvim-telescope/telescope.nvim',
+    lazy = false,
+    config = function()
+      require('plugins.telescope')
+    end,
+    dependencies = {
+      -- Telescope Extensions
+      { 'nvim-lua/popup.nvim' },
+      { 'nvim-lua/plenary.nvim' },
+      { 'kevinhwang91/nvim-bqf', ft = 'qf' },
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+      },
+      { 'cljoly/telescope-repo.nvim' },
+      { 'nvim-telescope/telescope-file-browser.nvim' },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+      { 'LinArcX/telescope-command-palette.nvim' },
+      { 'nvim-telescope/telescope-github.nvim' },
+      { 'dhruvmanila/telescope-bookmarks.nvim' },
+      { 'jvgrootveld/telescope-zoxide' },
+    },
+  },
+
+  {
+    'AckslD/nvim-neoclip.lua',
+    dependencies = {
+      -- you'll need at least one of these
+      { 'nvim-telescope/telescope.nvim' },
+      -- {'ibhagwan/fzf-lua'},
+    },
+    config = function()
+      require('neoclip').setup()
+    end,
+  },
+  --  { 'nvim-pack/nvim-spectre' },
+  {
+    'kyazdani42/nvim-tree.lua',
+    lazy = false,
+    config = function()
+      require('plugins.tree')
+    end,
+  },
+  {
+    'sidebar-nvim/sidebar.nvim',
+    config = function()
+      require('plugins.sidebar')
+    end,
+  },
+  {
+    'karb94/neoscroll.nvim',
+    config = function()
+      require('plugins.neoscroll')
+    end,
+  },
+  {
+    'gbprod/stay-in-place.nvim',
+    config = function()
+      require('stay-in-place').setup({})
+    end,
+  },
+  -- LSP Base
+  {
+    'williamboman/mason.nvim',
+    cmd = 'Mason',
+  },
+  { 'williamboman/mason-lspconfig.nvim' },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      {
+        'j-hui/fidget.nvim',
+        config = function()
+          require('fidget').setup({
+            window = {
+              relative = 'editor', -- where to anchor the window, either `"win"` or `"editor"`
+              blend = 0, -- `&winblend` for the window
+            },
+            text = {
+              spinner = 'dots',
+            },
+          })
+        end,
+      },
+      { 'folke/lua-dev.nvim' },
+    },
+  },
+  {
+    'stevearc/aerial.nvim',
+    config = function()
+      require('plugins.aerial')
+    end,
+  },
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = 'kevinhwang91/promise-async',
+    config = function()
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+      vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
+    end,
+  },
+
+  -- LSP Cmp
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    lazy = false,
+    dependencies = {
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-calc',
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      { 'tzachar/cmp-tabnine', build = './install.sh' },
+      {
+        'David-Kunz/cmp-npm',
+        config = function()
+          require('plugins.cmp-npm')
+        end,
+      },
+      -- {
+      -- 	'zbirenbaum/copilot-cmp',
+      -- 	disable = not NvimConfig.plugins.copilot.enabled,
+      -- 	config = function()
+      -- 	  require('copilot_cmp').setup()
+      -- 	end
+      -- },
+    },
+    config = function()
+      require('plugins.cmp')
+    end,
+  },
+  { 'onsails/lspkind-nvim' },
+  {
+    'NvChad/nvim-colorizer.lua',
+    config = function()
+      require('plugins.colorizer')
+    end,
+  },
+  { 'RRethy/vim-illuminate' },
+  {
+    'stevearc/dressing.nvim',
+    event = 'VeryLazy',
+    dependencies = 'MunifTanjim/nui.nvim',
+    config = function()
+      require('plugins.dressing')
+    end,
+  },
+  --
+  {
+    'folke/trouble.nvim',
+    cmd = { 'TroubleToggle', 'Trouble' },
+    config = function()
+      require('plugins.trouble')
+    end,
+  },
+  { 'nvim-lua/popup.nvim' },
+  {
+    'ChristianChiarulli/nvim-gps',
+    branch = 'text_hl',
+    config = function()
+      require('plugins.gps')
+    end,
+  },
+  { 'jose-elias-alvarez/typescript.nvim' },
+  {
+    'axelvc/template-string.nvim',
+    config = function()
+      require('template-string').setup()
+    end,
+  },
+  {
+    'lvimuser/lsp-inlayhints.nvim',
+    branch = 'anticonceal',
+    config = function()
+      require('lsp-inlayhints').setup()
+    end,
+  },
+
+  -- Formatter
+  {
+    'mhartington/formatter.nvim',
+    config = "require('plugins.formatter')",
+  },
+
+  --     -- General
+  -- 	{ "iamcco/markdown-preview.nvim",
+  -- 	run = "cd app && npm install",
+  -- 	setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+  -- 	ft = { "markdown" }, },
+
+  {
+    'airblade/vim-rooter',
+    setup = function()
+      vim.g.rooter_patterns = NvimConfig.plugins.rooter.patterns
+    end,
+  },
+  { 'AndrewRadev/switch.vim', lazy = false },
+  { 'AndrewRadev/splitjoin.vim' },
+  {
+    'numToStr/Comment.nvim',
+    branch = 'jsx',
+    config = function()
+      require('plugins.comment')
+    end,
+  },
+  { 'LudoPinelli/comment-box.nvim' },
+  {
+    'akinsho/nvim-toggleterm.lua',
+    config = function()
+      require('plugins.toggleterm')
+    end,
+    lazy = false,
+  },
+  { 'tpope/vim-repeat' },
+  { 'tpope/vim-speeddating' },
+  {
+    'Shatur/neovim-session-manager',
+    lazy = false,
+    config = function()
+      require('plugins.session-manager')
+    end,
+  },
+  {
+    'kylechui/nvim-surround',
+    config = function()
+      require('nvim-surround').setup({})
+    end,
+  },
+  {
+    'sunjon/shade.nvim',
+    config = function()
+      require('shade').setup()
+      require('shade').toggle()
+    end,
+  },
+
+  { 'dhruvasagar/vim-table-mode' },
+  {
+    'mg979/vim-visual-multi',
+    config = function()
+      vim.g.VM_leader = ';'
+    end,
+  },
+  {
+    'echasnovski/mini.nvim',
+    config = function()
+      require('mini.align').setup()
+    end,
+  },
+  {
+    'nacro90/numb.nvim',
+    lazy = false,
+    config = function()
+      require('plugins.numb')
+    end,
+  },
+  {
+    'folke/todo-comments.nvim',
+    config = function()
+      require('plugins.todo-comments')
+    end,
+  },
+  {
+    'folke/zen-mode.nvim',
+    config = function()
+      require('plugins.zen')
+    end,
+    disable = not NvimConfig.plugins.zen.enabled,
+  },
+  {
+    'folke/twilight.nvim',
+    config = function()
+      require('twilight').setup({})
+    end,
+    disable = not NvimConfig.plugins.zen.enabled,
+  },
+  {
+    'ggandor/lightspeed.nvim',
+    config = function()
+      require('plugins.lightspeed')
+    end,
+  },
+  {
+    'folke/which-key.nvim',
+    config = function()
+      require('plugins.which-key')
+    end,
+    lazy = false,
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    event = 'VimEnter',
+    config = function()
+      require('plugins.lualine')
+    end,
+  },
+  {
+    'romgrk/barbar.nvim',
+    dependencies = { 'kyazdani42/nvim-web-devicons' },
+    config = function()
+      require('plugins.barbar')
+    end,
+  },
+  { 'antoinemadec/FixCursorHold.nvim' }, -- Needed while issue https://github.com/neovim/neovim/issues/12587 is still open
+  { 'rcarriga/nvim-notify' },
+  {
+    'vuki656/package-info.nvim',
+    event = 'BufEnter package.json',
+    config = function()
+      require('plugins.package-info')
+    end,
+  },
+  --
+  {
+    'andweeb/presence.nvim',
+    config = function()
+      require('presence'):setup({
+        buttons = false,
+      })
+    end,
+  },
+
+  --     -- Snippets & Language & Syntax
+  -- 	{ 'Saecki/crates.nvim', dependencies = { 'nvim-lua/plenary.nvim' }, config = function()
+  -- 		require('plugins.crates') end
+  -- 	},
+  {
+    'windwp/nvim-autopairs',
+    config = function()
+      require('plugins.autopairs')
+    end,
+  },
+  { 'p00f/nvim-ts-rainbow' },
+  { 'mattn/emmet-vim' },
+  { 'potatoesmaster/i3-vim-syntax' },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    config = function()
+      require('plugins.indent')
+    end,
+  },
+
+  {
+    'danymat/neogen',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('plugins.neogen')
+    end,
+  },
+  { 'L3MON4D3/LuaSnip' },
+
+  --     -- {
+  --     --     "ThePrimeagen/refactoring.nvim",
+  --     --     config = "require('plugins.refactoring')";
+  --     --     dependencies = {
+  --     --         { "nvim-lua/plenary.nvim" },
+  --     --         { "nvim-treesitter/nvim-treesitter" },
+  --     --     },
+  --     -- },
+
+  --     -- Debug
+  --     { 'theHamsta/nvim-dap-virtual-text' },
+  --     { 'rcarriga/nvim-dap-ui' },
+  --     { 'mfussenegger/nvim-dap', config = function() require('plugins.dap') end },
+
+  --     -- Git
+  {
+    'lewis6991/gitsigns.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('plugins.gitsigns')
+    end,
+    event = 'BufRead',
+  },
+  {
+    'sindrets/diffview.nvim',
+    event = 'BufRead',
+    config = function()
+      require('plugins.diffview')
+    end,
+  },
+  --    { 'akinsho/git-conflict.nvim', config = function() require('plugins.git.conflict') end },
+  {
+    'ThePrimeagen/git-worktree.nvim',
+    config = function()
+      require('plugins.worktree')
+    end,
+  },
+  { 'kdheepak/lazygit.nvim', lazy = false },
+  {
+    'ruifm/gitlinker.nvim',
+    config = function()
+      require('plugins.gitlinker')
+    end,
+  },
+  {
+    'Pocco81/TrueZen.nvim',
+    cmd = { 'TZAtaraxis', 'TZMinimalist', 'TZFocus' },
+    config = function()
+      require('plugins.truezen')
+    end,
+  },
+}
