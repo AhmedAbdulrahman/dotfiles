f05c7f1792e22090cb908f7af826bf392434bcbf
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-02-19 17:23:47 +0100

    fix(plugins): lazy load vim-visual-multi

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -293,6 +293,15 @@ return {
   { 'dhruvasagar/vim-table-mode' },
   {
     'mg979/vim-visual-multi',
+    keys = {
+      '<C-n>',
+      '<C-Up>',
+      '<C-Down>',
+      '<S-Up>',
+      '<S-Down>',
+      '<S-Left>',
+      '<S-Right>',
+    },
     config = function()
       vim.g.VM_leader = ';'
     end,
