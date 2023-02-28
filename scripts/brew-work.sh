9a8faf390a1468de0c5cb59971b618795c89a390
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-03-01 00:30:18 +0100

    feat(brew): update work modules

--- scripts/brew-work.sh
+++ scripts/brew-work.sh
@@ -12,6 +12,10 @@ main() {
   # Vagrant-Manager helps you manage all your virtual machines in one place directly from the menubar
   brew_install "Vagrant Manager" "vagrant-manager" "homebrew/cask" "cask"
   brew_install "Ngrok - Reverse proxy, secure introspectable tunnels to localhost" "ngrok" "homebrew/cask" "cask"
+  brew_install "Colima ->" "colima"
+  brew_install "docker-credential-helper" "docker-credential-helper"
+  brew_install "docker-compose" "docker-compose"
+  brew_install "mkcert" "mkcert"
 
   # Remove outdated versions from the cellar
   brew cleanup
