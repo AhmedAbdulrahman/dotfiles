1a65dc0b7eeed211a4b1fec2de29ab7ad60c7db6
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-04-02 23:48:57 +0200

    feat(nvim/which-key): register jest keybindings

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -443,6 +443,27 @@ local function attach_zen(bufnr)
   })
 end
 
+local function attach_jest(bufnr)
+	wk.register({
+	  j = {
+		name = "Jest",
+		f = { '<cmd>lua require("neotest").run.run(vim.fn.expand("%"))<CR>', 'run current file' },
+		i = { '<cmd>lua require("neotest").summary.toggle()<CR>', 'toggle info panel' },
+		j = { '<cmd>lua require("neotest").run.run()<CR>', 'run nearest test' },
+		l = { '<cmd>lua require("neotest").run.run_last()<CR>', 'run last test' },
+		o = { '<cmd>lua require("neotest").output.open({ enter = true })<CR>', 'open test output'},
+		s = { '<cmd>lua require("neotest").run.stop()<CR>', 'stop' },
+	  }
+	}, {
+	  buffer = bufnr,
+	  mode = "n", -- NORMAL mode
+	  prefix = "<leader>",
+	  silent = true, -- use `silent` when creating keymaps
+	  noremap = true, -- use `noremap` when creating keymaps
+	  nowait = false, -- use `nowait` when creating keymaps
+	})
+end
+
 local function attach_spectre(bufnr)
   wk.register({
     ['R'] = { '[SPECTRE] Replace all' },
@@ -486,6 +507,7 @@ return {
   attach_markdown = attach_markdown,
   attach_typescript = attach_typescript,
   attach_zen = attach_zen,
+  attach_jest = attach_jest,
   attach_spectre = attach_spectre,
   attach_crates = attach_crates,
 }
