4849baf020124900fc676e2de1dfc34de3f122a8
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-04-05 00:18:50 +0200

    feat(lsp): enable TypeScript codeactions for null-ls by default

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -6,5 +6,9 @@ require("mason-null-ls").setup({
     automatic_setup = true, -- Recommended, but optional
 })
 
-require("null-ls").setup()
+require("null-ls").setup({
+	sources = {
+		require("typescript.extensions.null-ls.code-actions"),
+	},
+})
 require "mason-null-ls".setup_handlers() -- If `automatic_setup` is true.
