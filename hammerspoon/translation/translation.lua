b92713a900bbd6e20c8ea240115e1411242dd1cf
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-04-05 05:33:34 +0200

    fix(hammerspoon): windowMasks for translation

--- hammerspoon/translation/translation.lua
+++ hammerspoon/translation/translation.lua
@@ -1,11 +1,11 @@
-Install:andUse("PopupTranslateSelection",
-        {
-            config = {
-                popup_style = wm.utility|wm.HUD|wm.titled|
-                        wm.closable|wm.resizable,
-            },
-            hotkeys = {
-                translate_to_en = { {"cmd", "shift", "ctrl"}, "z" },
-            }
-        }
-)
+-- luacheck: globals hs Install
+local wm = hs.webview.windowMasks
+
+Install:andUse('PopupTranslateSelection', {
+  config = {
+    popup_style = wm.utility | wm.HUD | wm.titled | wm.closable | wm.resizable,
+  },
+  hotkeys = {
+    translate_to_en = { { 'cmd', 'shift', 'ctrl' }, 'z' },
+  },
+})
