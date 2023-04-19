daf513a0dd716ffe4b645c5e61779ac3fe4a6c53
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-04-19 17:11:17 +0200

    fix(null-ls): removing setup_handlers function automatic_setup is now implicitly true

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,14 +1,14 @@
-require("mason-null-ls").setup({
-    ensure_installed = {
-        -- Opt to list sources here, when available in mason.
-    },
-    automatic_installation = false,
-    automatic_setup = true, -- Recommended, but optional
+require('mason-null-ls').setup({
+  ensure_installed = {
+    -- Opt to list sources here, when available in mason.
+  },
+  automatic_installation = false,
+  automatic_setup = true, -- Recommended, but optional
 })
 
-require("null-ls").setup({
-	sources = {
-		require("typescript.extensions.null-ls.code-actions"),
-	},
+require('null-ls').setup({
+  sources = {
+    require('typescript.extensions.null-ls.code-actions'),
+  },
 })
-require "mason-null-ls".setup_handlers() -- If `automatic_setup` is true.
+-- require "mason-null-ls".setup_handlers() -- If `automatic_setup` is true.
