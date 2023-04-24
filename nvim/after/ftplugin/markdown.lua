5d3b96cc0b6c43ac2fdff4fe0d272ee2413453e1
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-04-24 03:37:23 +0200

    chore(nvim/markdown): remap markdown preview

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -2,10 +2,10 @@ local utils = require('utils')
 
 utils.plaintext()
 
-if vim.fn.executable 'gh' == 1 then
-  vim.cmd [[nnoremap <buffer><leader>p :call ahmed#utils#openMarkdownPreview()<CR>]]
+if vim.fn.executable('gh') == 1 then
+  vim.cmd([[nnoremap <buffer><leader>op :call ahmed#utils#openMarkdownPreview()<CR>]])
 end
 
-if vim.fn.executable 'glow' == 1 then
-  vim.cmd [[nnoremap <buffer><leader>g :Glow<CR>]]
+if vim.fn.executable('glow') == 1 then
+  vim.cmd([[nnoremap <buffer><leader>g :Glow<CR>]])
 end
