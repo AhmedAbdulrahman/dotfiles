6de410014fd5e46daffff08269cfc5af5a756e76
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-08-14 10:04:40 +0200

    feat(nvim/plugins): enable min_chars in tailwind-fold plugin

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -241,7 +241,9 @@ return {
   },
   {
     'razak17/tailwind-fold.nvim',
-    opts = {},
+    opts = {
+      min_chars = 50,
+    },
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     ft = { 'html', 'svelte', 'astro', 'vue', 'typescriptreact' },
   },
