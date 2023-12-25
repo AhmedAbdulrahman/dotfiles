2a19404864f8f215acd450624371cfdbd88ebf1f
Ahmed Abdulrahman <a.kasapbashi@live.com>
2023-12-25 11:15:33 +0100

    feat(nvim/lsp): vim.lsp.inlay_hint was changed to vim.lsp.inlay_hint.enable in neovim 10

--- nvim/lua/lsp/functions.lua
+++ nvim/lua/lsp/functions.lua
@@ -63,4 +63,9 @@ function M.custom_hover_handler(_, _result)
   return handler
 end
 
+function M.toggle_inlay_hints()
+  local bufnr = vim.api.nvim_get_current_buf()
+  vim.lsp.inlay_hint.enable(bufnr, not vim.lsp.inlay_hint.is_enabled(bufnr))
+end
+
 return M
