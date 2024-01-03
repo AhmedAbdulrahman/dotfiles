260e31ad8edcddde559846e6de9b14b3fdb6ac0c
Ahmed Abdulrahman <a.kasapbashi@live.com>
2024-01-03 16:37:30 +0100

    feat(nvim/autopairs): update config

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,19 +1,33 @@
 require('nvim-autopairs').setup({
     check_ts = true, -- enable treesitter
     ts_config = {
-      lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-      javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
+      lua = { "string", "source" }, -- don't add pairs in lua string treesitter nodes
+      javascript = { "string", "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
       java = false, -- don't check treesitter on java
     },
-    fast_wrap = {},
-    disable_filetype = { "TelescopePrompt", "vim" },
+    fast_wrap = {
+      map = "<M-e>",
+      chars = { "{", "[", "(", '"', "'" },
+      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+      offset = 0, -- Offset from pattern match
+      end_key = "$",
+      keys = "qwertyuiopzxcvbnmasdfghjkl",
+      check_comma = true,
+      highlight = "PmenuSel",
+      highlight_grey = "LineNr",
+    },
+    disable_filetype = { "TelescopePrompt", "vim", "spectre_panel" },
 })
 
 -- import nvim-autopairs completion functionality
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
 -- import nvim-cmp plugin (completions plugin)
-local cmp = require('cmp')
+local cmp_status_ok, cmp = pcall(require, "cmp")
+
+if not cmp_status_ok then
+  return
+end
 
 -- make autopairs and completion work together
 cmp.event:on(
