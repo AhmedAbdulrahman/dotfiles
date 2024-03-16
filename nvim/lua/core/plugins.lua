ff4e81125217b75f44e003c2a5e08921a5d01541
Ahmed Abdulrahman <a.kasapbashi@live.com>
2024-03-16 14:31:28 +0100

    feat(plugins): add copilot chat

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -894,15 +894,56 @@ return {
     cond = NvimConfig.plugins.ai.copilot.enabled,
     cmd = "Copilot",
     build = ":Copilot auth",
-    event = "InsertEnter",
+    lazy = false,
     config = function()
       require('plugins.copilot')
     end,
   },
   {
-    'madox2/vim-ai',
-    cond = NvimConfig.plugins.ai.vim_ai.enabled,
-    lazy = false,
+    "CopilotC-Nvim/CopilotChat.nvim",
+    event = "VeryLazy",
+    opts = {
+      show_help = "no",
+      prompts = {
+        Explain = "Explain how it works.",
+        Review = "Review the following code and provide concise suggestions.",
+        Tests = "Briefly explain how the selected code works, then generate unit tests.",
+        Refactor = "Refactor the code to improve clarity and readability.",
+      },
+    },
+    build = function()
+      vim.defer_fn(function()
+        vim.cmd("UpdateRemotePlugins")
+        vim.notify("CopilotChat - Updated remote plugins. Please restart Neovim.")
+      end, 3000)
+    end,
+    keys = {
+      { "<leader>ccb", ":CopilotChatBuffer<cr>",      desc = "CopilotChat - Buffer" },
+      { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+      { "<leader>cct", "<cmd>CopilotChatTests<cr>",   desc = "CopilotChat - Generate tests" },
+      {
+        "<leader>ccT",
+        "<cmd>CopilotChatVsplitToggle<cr>",
+        desc = "CopilotChat - Toggle Vsplit", -- Toggle vertical split
+      },
+      {
+        "<leader>ccv",
+        ":CopilotChatVisual",
+        mode = "x",
+        desc = "CopilotChat - Open in vertical split",
+      },
+      {
+        "<leader>ccc",
+        ":CopilotChatInPlace<cr>",
+        mode = { "n", "x" },
+        desc = "CopilotChat - Run in-place code",
+      },
+      {
+        "<leader>ccf",
+        "<cmd>CopilotChatFixDiagnostic<cr>", -- Get a fix for the diagnostic message under the cursor.
+        desc = "CopilotChat - Fix diagnostic",
+      },
+    }
   },
   {
     'jackMort/ChatGPT.nvim',
