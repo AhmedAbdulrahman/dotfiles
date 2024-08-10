8460d3bcf7864f8d03b0f373b130a0d495d48caa
Ahmed Abdulrahman <a.kasapbashi@live.com>
2024-08-10 10:28:23 +0200

    feat(nvim): nvim-highlight-colors plugin

--- nvim/lua/lsp/servers/tailwindcss.lua
+++ nvim/lua/lsp/servers/tailwindcss.lua
@@ -10,16 +10,6 @@ capabilities.textDocument.foldingRange = {
 
 -- Settings
 
-local on_attach = function(client, bufnr)
-	if client.server_capabilities.colorProvider then
-		require("lsp/utils/documentcolors").buf_attach(bufnr)
-		require("colorizer").attach_to_buffer(
-			bufnr,
-			{ mode = "background", css = true, names = false, tailwind = false }
-		)
-	end
-end
-
 local filetypes = { "html", "mdx", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "svelte" }
 
 local init_options = {
