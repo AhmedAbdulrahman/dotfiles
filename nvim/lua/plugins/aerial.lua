3b083c6370c954290bddf9bc3860e50d7fab5fc0
Ahmed Abdulrahman <a.kasapbashi@live.com>
2024-11-03 10:13:47 +0100

    feat(nvim/aerial): change config format

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -1,76 +1,63 @@
-local lazy = require('lazy')
-local ftplugin = lazy.require('core.ftplugin')
+local lazy_aerial = setmetatable({}, {
+  __index = function(_, k)
+    return function(...) require("aerial")[k](...) end
+  end,
+})
 
-lazy.require('aerial', function(aerial)
-  ftplugin.extend('aerial', {
-    ignore_win_opts = true,
-  })
-  aerial.setup({
-    show_guides = true,
-    layout = {
-      max_width = { 80, 0.2 },
-      default_direction = 'prefer_left',
-      -- placement = "edge",
-      preserve_equality = true,
+return {
+  {
+    'stevearc/aerial.nvim',
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+      -- Hack to ensure that lspkind-nvim is loaded
+      "hrsh7th/nvim-cmp",
     },
-    -- attach_mode = "global",
-    highlight_on_hover = true,
-    close_automatic_events = {
-      -- "unfocus",
-      -- "switch_buffer",
-      -- "unsupported",
+    cmd = { "AerialToggle", "AerialOpen", "AerialNext", "AerialPrev" },
+    keys = {
+      { "<leader>a", "<cmd>AerialToggle!<CR>", desc = "[A]erial toggle", mode = "n" },
+      { "<leader>A", "<cmd>AerialNavToggle<CR>", desc = "[A]erial nav toggle", mode = "n" },
+      { "[s", lazy_aerial.prev, desc = "Previous aerial symbol", mode = { "n", "v" } },
+      { "]s", lazy_aerial.next, desc = "Next aerial symbol", mode = { "n", "v" } },
+      { "[u", lazy_aerial.prev_up, desc = "Previous aerial parent symbol", mode = { "n", "v" } },
+      { "]u", lazy_aerial.next_up, desc = "Next aerial parent symbol", mode = { "n", "v" } },
     },
-    -- open_automatic = true,
-    -- highlight_on_jump = false,
-    link_folds_to_tree = true,
-    link_tree_to_folds = true,
-    manage_folds = {
-      ['_'] = true,
-    },
-    nerd_font = vim.g.nerd_font,
+    opts = {
+      show_guides = true,
+      layout = {
+        max_width = { 80, 0.2 },
+        default_direction = "prefer_left",
+        -- placement = "edge",
+        -- preserve_equality = true,
+      },
+      -- attach_mode = "global",
+      highlight_on_hover = true,
+      close_automatic_events = {
+        -- "unfocus",
+        -- "switch_buffer",
+        -- "unsupported",
+      },
+      -- open_automatic = true,
+      -- highlight_on_jump = false,
+      link_folds_to_tree = true,
+      link_tree_to_folds = true,
+      manage_folds = {
+        ["_"] = true,
+      },
+      nerd_font = vim.g.nerd_font,
 
-    -- backends = { "treesitter", "markdown" },
-    -- backends = { "lsp", "treesitter" },
-    lazy_load = true,
-    -- backends = { "lsp", "treesitter", "markdown" },
-    -- filter_kind = false,
-    on_attach = function() -- bufnr
-      -- vim.keymap.set({ "n", "v" }, "{", aerial.prev, { buffer = bufnr })
-      -- vim.keymap.set({ "n", "v" }, "}", aerial.next, { buffer = bufnr })
-    end,
-    keymaps = {
-      ['<'] = 'actions.tree_decrease_fold_level',
-      ['>'] = 'actions.tree_increase_fold_level',
+      -- backends = { "treesitter", "markdown" },
+      -- backends = { "lsp", "treesitter" },
+      lazy_load = false,
+      -- backends = { "lsp", "treesitter", "markdown" },
+      -- filter_kind = false,
+      keymaps = {
+        ["<"] = "actions.tree_decrease_fold_level",
+        [">"] = "actions.tree_increase_fold_level",
+      },
+      treesitter = {
+        experimental_selection_range = true,
+      },
     },
-  })
-  vim.keymap.set(
-    'n',
-    '<leader>a',
-    '<cmd>AerialToggle!<CR>',
-    { desc = '[A]erial toggle' }
-  )
-  vim.keymap.set(
-    { 'n', 'v' },
-    '[s',
-    aerial.prev,
-    { desc = 'Previous aerial symbol' }
-  )
-  vim.keymap.set(
-    { 'n', 'v' },
-    ']s',
-    aerial.next,
-    { desc = 'Next aerial symbol' }
-  )
-  vim.keymap.set(
-    { 'n', 'v' },
-    '[u',
-    aerial.prev_up,
-    { desc = 'Previous aerial parent symbol' }
-  )
-  vim.keymap.set(
-    { 'n', 'v' },
-    ']u',
-    aerial.next_up,
-    { desc = 'Next aerial parent symbol' }
-  )
-end)
+  },
+}
