b7ce7b04bb1e85c91b62727bba1a3dfb1b68ee3e
Ahmed Abdulrahman <a.kasapbashi@live.com>
2024-11-03 10:11:39 +0100

    feat(nvim/notify): update icons & change position

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -2,8 +2,23 @@ return {
   {
     "rcarriga/nvim-notify",
     lazy = false,
+    event = "VeryLazy",
     config = function()
       require("notify").setup({
+        fps = 30,
+        icons = {
+          DEBUG = "",
+          ERROR = "",
+          INFO = "",
+          TRACE = "✎",
+          WARN = "",
+        },
+        level = 2,
+        max_width = 80,
+        render = "minimal",
+        -- stages = "fade_in_slide_out",
+        timeout = 5000,
+        top_down = true,
         -- Animation style
         stages = "fade_in_slide_out",
         -- Default timeout for notifications
